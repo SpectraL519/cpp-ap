2ec64152b62f70b3335964b7a31ea2a443d6f207
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-10-29 16:08:35 +0100

    const argument_interface functions + format

--- include/ap/argument_parser.hpp
+++ include/ap/argument_parser.hpp
@@ -8,31 +8,29 @@ namespace ap {
 namespace detail {
 
 template <typename T>
-concept readable = requires (T value, std::istream& input_stream) {
-    input_stream >> value;
-};
+concept readable =
+    requires(T value, std::istream& input_stream) { input_stream >> value; };
 
 
 template <readable T>
 class argument_interface {
 public:
-    using value_type = T;
-
-    virtual const std::string& name() = 0;
-
-    virtual bool is_positional() = 0;
-    virtual bool is_optional() = 0;
-
-    virtual bool has_value() = 0;
-    virtual const value_type& value() = 0;
+    using value_type = std::remove_reference_t<T>;
 
     virtual argument_interface& help(const std::string&) = 0;
     virtual argument_interface& required(bool) = 0;
     virtual argument_interface& default_value(const value_type&) = 0;
 
-    virtual const std::string& help() = 0;
-    virtual bool required() = 0;
-    virtual const value_type& default_value() = 0;
+    virtual bool is_positional() const = 0;
+    virtual bool is_optional() const = 0;
+
+    virtual bool has_value() const = 0;
+    virtual const value_type& value() const = 0;
+
+    virtual const std::string& name() const = 0;
+    virtual const std::string& help() const = 0;
+    virtual bool required() const = 0;
+    virtual const value_type& default_value() const = 0;
 
     virtual ~argument_interface() = default;
 };
