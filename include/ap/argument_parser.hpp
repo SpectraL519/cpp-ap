23f468f7a1fb9c5edec876d5484ad29cbc6245a6
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-10-30 19:25:48 +0100

    added positional_argument function implementations and corresponding unit tests

--- include/ap/argument_parser.hpp
+++ include/ap/argument_parser.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <optional>
+#include <string_view>
 
 
 namespace ap {
@@ -12,14 +14,20 @@ concept readable =
     requires(T value, std::istream& input_stream) { input_stream >> value; };
 
 
+
 template <readable T>
 class argument_interface {
 public:
     using value_type = std::remove_reference_t<T>;
 
-    virtual argument_interface& help(const std::string&) = 0;
+    virtual argument_interface& help(std::string_view) = 0;
     virtual argument_interface& required(bool) = 0;
     virtual argument_interface& default_value(const value_type&) = 0;
+
+    virtual ~argument_interface() = default;
+
+protected:
+    virtual argument_interface& value(const value_type&) = 0;
 
     virtual bool is_positional() const = 0;
     virtual bool is_optional() const = 0;
@@ -27,37 +35,123 @@ public:
     virtual bool has_value() const = 0;
     virtual const value_type& value() const = 0;
 
-    virtual const std::string& name() const = 0;
-    virtual const std::string& help() const = 0;
+    virtual const std::string_view name() const = 0;
     virtual bool required() const = 0;
-    virtual const value_type& default_value() const = 0;
+    virtual const std::optional<std::string_view>& help() const = 0;
+    virtual const std::optional<value_type>& default_value() const = 0;
 
-    virtual ~argument_interface() = default;
+#ifdef AP_TESTING
+    template <readable U>
+    friend inline bool testing_is_positional(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline bool testing_is_optional(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline bool testing_has_value(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline const typename argument_interface<U>::value_type&
+        testing_get_value(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline const std::string_view
+        testing_get_name(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline bool testing_is_required(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline const std::optional<std::string_view>&
+        testing_get_help(const argument_interface<U>&);
+
+    template <readable U>
+    friend inline const std::optional<typename argument_interface<U>::value_type>&
+        testing_get_default_value(const argument_interface<U>&);
+#endif
 };
 
 
 template <readable T>
 class positional_argument : public argument_interface<T> {
 public:
-    using value_type = std::remove_reference_t<T>;
+    using value_type = typename argument_interface<T>::value_type;
 
-    positional_argument() = default;
+    positional_argument() = delete;
+
+    explicit positional_argument(std::string_view name) : _name(name) {}
+
     ~positional_argument() = default;
 
-    [[nodiscard]] argument_interface& help(const std::string&) override;
-    [[nodiscard]] argument_interface& required(bool) override;
-    [[nodiscard]] argument_interface& default_value(const value_type&) override;
+    bool operator== (const positional_argument& other) const {
+        return this->_name == other._name;
+    }
 
-    [[nodiscard]] bool is_positional() const override;
-    [[nodiscard]] bool is_optional() const override;
+    inline positional_argument& help(std::string_view help_msg) override {
+        this->_help_msg = help_msg;
+        return *this;
+    }
 
-    [[nodiscard]] bool has_value() const override;
-    [[nodiscard]] const value_type& value() const override;
+    inline positional_argument& required(bool required) override {
+        this->_required = required;
+        return *this;
+    }
 
-    [[nodiscard]] const std::string& name() const override;
-    [[nodiscard]] const std::string& help() const override;
-    [[nodiscard]] bool required() const override;
-    [[nodiscard]] const value_type& default_value() const override;
+    inline positional_argument& default_value(const value_type& default_value) override {
+        this->_default_value = default_value;
+        return *this;
+    }
+
+private:
+    inline positional_argument& value(const value_type& value) override {
+        this->_value = value;
+        return *this;
+    }
+
+    [[nodiscard]] inline bool is_positional() const override {
+        return not this->_optional;
+    }
+
+    [[nodiscard]] inline bool is_optional() const override {
+        return this->_optional;
+    }
+
+    [[nodiscard]] bool has_value() const override {
+        return this->_value.has_value();
+    }
+
+    [[nodiscard]] const value_type& value() const override {
+        return this->_value.value();
+    }
+
+    [[nodiscard]] const std::string_view name() const override {
+        return this->_name;
+    }
+
+    [[nodiscard]] bool required() const override { return this->_required; }
+
+    [[nodiscard]] const std::optional<std::string_view>& help() const override {
+        return this->_help_msg;
+    }
+
+    [[nodiscard]] const std::optional<value_type>& default_value() const override {
+        return this->_default_value;
+    }
+
+    const bool _optional = false;
+    const std::string_view _name;
+
+    std::optional<value_type> _value;
+
+    bool _required = true;
+    std::optional<std::string_view> _help_msg;
+    std::optional<value_type> _default_value;
+
+#ifdef AP_TESTING
+    template <readable U>
+    friend inline positional_argument<U>&
+        testing_set_value(positional_argument<U>&, const typename positional_argument<U>::value_type&);
+#endif
 };
 
 } // namespace detail
