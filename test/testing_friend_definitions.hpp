d71a98836adbb70d56c1ced99803ea7013d417de
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-11-03 23:37:34 +0100

    PWRS5PZ-4: Create the positional argument class

---
+++ test/testing_friend_definitions.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <ap/argument_parser.hpp>
+
+#ifdef AP_TESTING
+
+namespace ap::detail {
+
+template <readable T>
+using arg_value_type = typename argument_interface<T>::value_type;
+
+template <readable T>
+bool testing_is_positional(const argument_interface<T>& argument) {
+    return argument.is_positional();
+}
+
+template <readable T>
+bool testing_is_optional(const argument_interface<T>& argument) {
+    return argument.is_optional();
+}
+
+template <readable T>
+bool testing_has_value(const argument_interface<T>& argument) {
+    return argument.has_value();
+}
+
+template <readable T>
+const arg_value_type<T>& testing_get_value(const argument_interface<T>& argument) {
+    return argument.value();
+}
+
+template <readable T>
+const std::string_view testing_get_name(const argument_interface<T>& argument) {
+    return argument.name();
+}
+
+template <readable T>
+bool testing_is_required(const argument_interface<T>& argument) {
+    return argument.required();
+}
+
+template <readable T>
+const std::optional<std::string_view>&
+    testing_get_help(const argument_interface<T>& argument) {
+    return argument.help();
+}
+
+template <readable T>
+const std::optional<typename argument_interface<T>::value_type>&
+    testing_get_default_value(const argument_interface<T>& argument) {
+    return argument.default_value();
+}
+
+
+template <readable T>
+using positional_arg_value_type = typename positional_argument<T>::value_type;
+
+template <readable T>
+positional_argument<T>& testing_set_value(
+    positional_argument<T>& argument, const positional_arg_value_type<T>& value
+) {
+    return argument.value(value);
+}
+
+} // namespace ap::detail
+
+#endif
---
+++ test/tests/test_positional_argument.cpp
@@ -0,0 +1,196 @@
+#define AP_TESTING
+
+#include "../doctest.h"
+#include "../testing_friend_definitions.hpp"
+
+#include <ap/argument_parser.hpp>
+
+#include <string_view>
+
+using namespace ap::detail;
+
+
+namespace {
+
+using test_value_type = int;
+
+constexpr std::string_view argument_name = "test name";
+
+
+positional_argument<test_value_type> default_positional_argument() {
+    return positional_argument<test_value_type>(argument_name);
+}
+
+} // namespace
+
+
+
+TEST_CASE("positonal argument should be positional and not optional") {
+    const auto argument = default_positional_argument();
+
+    REQUIRE(testing_is_positional(argument));
+    REQUIRE_FALSE(testing_is_optional(argument));
+}
+
+TEST_CASE("has_value() should return false by default") {
+    const auto argument = default_positional_argument();
+
+    REQUIRE_FALSE(testing_has_value(argument));
+}
+
+TEST_CASE("has_value() should return true is value is set") {
+    auto argument = default_positional_argument();
+
+    test_value_type value{};
+    testing_set_value(argument, value);
+
+    REQUIRE(testing_has_value(argument));
+}
+
+TEST_CASE("value() should throw if argument's value has not been set") {
+    auto argument = default_positional_argument();
+
+    REQUIRE_FALSE(testing_has_value(argument));
+    REQUIRE_THROWS_AS(testing_get_value(argument), std::bad_optional_access);
+}
+
+TEST_CASE("value() should return the argument's value if it has been set") {
+    auto argument = default_positional_argument();
+
+    test_value_type value{};
+    testing_set_value(argument, value);
+
+    REQUIRE(testing_has_value(argument));
+    REQUIRE_EQ(testing_get_value(argument), value);
+}
+
+TEST_CASE("name() should return value passed to the positonal argument "
+          "constructor") {
+    const auto argument = default_positional_argument();
+
+    REQUIRE_EQ(testing_get_name(argument), argument_name);
+}
+
+TEST_CASE("required() should return true by default") {
+    auto argument = default_positional_argument();
+
+    REQUIRE(testing_is_required(argument));
+}
+
+TEST_CASE("required() should return the value it has been set to") {
+    auto argument = default_positional_argument();
+    bool required;
+
+    SUBCASE("set to true") {
+        required = true;
+    }
+    SUBCASE("set to false") {
+        required = false;
+    }
+
+    CAPTURE(required);
+
+    argument.required(required);
+
+    REQUIRE_EQ(testing_is_required(argument), required);
+}
+
+TEST_CASE("help() should return nullopt by default") {
+    const auto argument = default_positional_argument();
+
+    REQUIRE_FALSE(testing_get_help(argument));
+}
+
+TEST_CASE("help() should return message if one has been provided") {
+    auto argument = default_positional_argument();
+
+    constexpr std::string_view help_msg = "test help msg";
+    argument.help(help_msg);
+
+    const auto returned_help_msg = testing_get_help(argument);
+
+    REQUIRE(returned_help_msg);
+    REQUIRE_EQ(returned_help_msg, help_msg);
+}
+
+TEST_CASE("defaul_value() should return nullopt by default") {
+    const auto argument = default_positional_argument();
+
+    REQUIRE_FALSE(testing_get_default_value(argument));
+}
+
+TEST_CASE("defaul_value() should return value if one has been provided") {
+    auto argument = default_positional_argument();
+
+    test_value_type default_value{};
+    argument.default_value(default_value);
+
+    const auto returned_default_value = testing_get_default_value(argument);
+
+    REQUIRE(returned_default_value);
+    REQUIRE_EQ(returned_default_value, default_value);
+}
+
+
+// positional_argument class' setter functions
+
+TEST_CASE("value(const value_type&) should set value and return the argument "
+          "instance") {
+    auto argument = default_positional_argument();
+
+    test_value_type value{};
+
+    const auto returned_argument = testing_set_value(argument, value);
+
+    REQUIRE(testing_has_value(argument));
+    REQUIRE_EQ(testing_get_value(argument), value);
+    REQUIRE_EQ(returned_argument, argument);
+}
+
+TEST_CASE("required(bool) should set required attribute and return the argument") {
+    auto argument = default_positional_argument();
+    bool required;
+
+    SUBCASE("set to true") {
+        required = true;
+    }
+    SUBCASE("set to false") {
+        required = false;
+    }
+
+    CAPTURE(required);
+
+    const auto returned_argument = argument.required(required);
+
+    REQUIRE_EQ(testing_is_required(argument), required);
+    REQUIRE_EQ(returned_argument, argument);
+}
+
+TEST_CASE("help(string_view) should set help message and return the argument") {
+    auto argument = default_positional_argument();
+
+    constexpr std::string_view help_msg = "test help msg";
+
+    const auto returned_argument = argument.help(help_msg);
+
+    const auto returned_help_msg = testing_get_help(argument);
+
+    REQUIRE(returned_help_msg);
+    REQUIRE_EQ(returned_help_msg, help_msg);
+    REQUIRE_EQ(returned_argument, argument);
+}
+
+TEST_CASE("default_value(string_view) should set help message and return the "
+          "argument") {
+    auto argument = default_positional_argument();
+
+    test_value_type default_value{};
+
+    const auto returned_argument = argument.default_value(default_value);
+
+    const auto returned_default_value = testing_get_default_value(argument);
+
+    REQUIRE(returned_default_value);
+    REQUIRE_EQ(returned_default_value, default_value);
+    REQUIRE_EQ(returned_argument, argument);
+}
