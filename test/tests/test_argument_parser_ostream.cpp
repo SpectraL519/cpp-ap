f6ec82391b3158798a50a5ac9a94f6eee991737c
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-11-10 16:10:55 +0100

    PWRS5PZ-22: Bug fix - argument parser ostream operator tests failing on ubuntu

--- test/tests/test_argument_parser_ostream.cpp
+++ test/tests/test_argument_parser_ostream.cpp
@@ -15,27 +15,24 @@ TEST_CASE("operator<< should push correct data to the output stream") {
     std::stringstream ss;
     std::stringstream expected_ss;
 
+    const std::string test_name = "test program name";
+    const std::string test_description = "test program description";
+
     SUBCASE("nothing by default") {}
 
     SUBCASE("only program name if nothing else set") {
-        std::string name = "test program name";
-        parser.program_name(name);
-        expected_ss << name << std::endl;
+        parser.program_name(test_name);
+        expected_ss << test_name << std::endl;
     }
 
     SUBCASE("only program description if nothing else set") {
-        std::string description = "test program description";
-        parser.program_description(description);
-        expected_ss << description << std::endl;
+        parser.program_description(test_description);
+        expected_ss << test_description << std::endl;
     }
 
     SUBCASE("program name and description if both specified") {
-        std::string name = "test program name";
-        std::string description = "test program description";
-
-        parser.program_name(name).program_description(description);
-
-        expected_ss << name << std::endl << description << std::endl;
+        parser.program_name(test_name).program_description(test_description);
+        expected_ss << test_name << std::endl << test_description << std::endl;
     }
 
     CAPTURE(expected_ss);
