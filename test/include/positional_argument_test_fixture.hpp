7d472f133ca2708b00d833ff7c79c0c2511ec395
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-12-28 12:49:06 +0100

    PWRS5PZ-35: Default arguments
    Added:
       - the `add_flag<bool>(name)` function to the parser
       - the `ap::defeault_argument` struct
       - the `default_{positional/optional}_arguments(vector<default_arg_specifier>)` functions to the argument parser
       - explicit enum class underlying types
       - tests covering the added functionality
    Moved the `argument_name` class to the `ap::argument::detail` namespace

--- test/include/positional_argument_test_fixture.hpp
+++ test/include/positional_argument_test_fixture.hpp
@@ -6,7 +6,7 @@
 
 #include <cstring>
 
-using ap::argument::argument_name;
+using ap::argument::detail::argument_name;
 using ap::argument::positional_argument;
 using ap::utility::readable;
 
@@ -29,7 +29,7 @@ struct positional_argument_test_fixture {
     template <readable T>
     inline positional_argument<T>& sut_set_choices(
         positional_argument<T>& sut, const std::vector<value_type<T>>& choices
-    ) {
+    ) const {
         return sut.choices(choices);
     }
 
--- test/src/test_argument_parser_add_argument.cpp
+++ test/src/test_argument_parser_add_argument.cpp
@@ -2,6 +2,7 @@
 
 #include "doctest.h"
 #include "argument_parser_test_fixture.hpp"
+#include "optional_argument_test_fixture.hpp"
 
 #include <ap/argument_parser.hpp>
 
@@ -27,36 +28,50 @@ TEST_SUITE_BEGIN("test_argument_parser_add_argument");
 
 TEST_CASE_FIXTURE(
     argument_parser_test_fixture,
-    "add_positional_argument should return a positional argument reference"
+    "default_positional_arguments should add the specified arguments"
 ) {
-    SUBCASE("with just the long name") {
-        const auto& argument = sut.add_positional_argument(name);
-        REQUIRE_FALSE(argument.is_optional());
-    }
-    SUBCASE("with both names") {
-        const auto& argument = sut.add_positional_argument(name, short_name);
-        REQUIRE_FALSE(argument.is_optional());
-    }
+    sut.default_positional_arguments({
+        ap::default_argument::positional::input,
+        ap::default_argument::positional::output
+    });
+
+    REQUIRE_FALSE(sut_get_argument("input")->get().is_optional());
+    REQUIRE_FALSE(sut_get_argument("output")->get().is_optional());
 }
 
 TEST_CASE_FIXTURE(
     argument_parser_test_fixture,
-    "add_optional_argument should return a positional argument reference"
+    "default_optional_arguments should add the specified arguments"
 ) {
-    SUBCASE("with just the long name") {
-        const auto& argument = sut.add_optional_argument(name);
-        REQUIRE(argument.is_optional());
-    }
-    SUBCASE("with both names") {
-        const auto& argument = sut.add_optional_argument(name, short_name);
-        REQUIRE(argument.is_optional());
-    }
+    sut.default_optional_arguments({
+        ap::default_argument::optional::help,
+        ap::default_argument::optional::input,
+        ap::default_argument::optional::output
+    });
+
+    REQUIRE(sut_get_argument("help")->get().is_optional());
+    REQUIRE(sut_get_argument("h")->get().is_optional());
+
+    REQUIRE(sut_get_argument("input")->get().is_optional());
+    REQUIRE(sut_get_argument("i")->get().is_optional());
+
+    REQUIRE(sut_get_argument("output")->get().is_optional());
+    REQUIRE(sut_get_argument("o")->get().is_optional());
+}
+
+
+TEST_CASE_FIXTURE(
+    argument_parser_test_fixture,
+    "add_positional_argument should return a positional argument reference"
+) {
+    const auto& argument = sut.add_positional_argument(name, short_name);
+    REQUIRE_FALSE(argument.is_optional());
 }
 
 TEST_CASE_FIXTURE(
     argument_parser_test_fixture,
     "add_positional_argument should throw only when adding an"
-    "argument with previously used name"
+    "argument with a previously used name"
 ) {
     sut.add_positional_argument(name, short_name);
 
@@ -65,7 +80,8 @@ TEST_CASE_FIXTURE(
     }
 
     SUBCASE("adding argument with a previously used long name") {
-        REQUIRE_THROWS_AS(sut.add_positional_argument(name), std::invalid_argument);
+        REQUIRE_THROWS_AS(
+            sut.add_positional_argument(name, other_short_name), std::invalid_argument);
     }
 
     SUBCASE("adding argument with a previously used short name") {
@@ -75,10 +91,19 @@ TEST_CASE_FIXTURE(
     }
 }
 
+
+TEST_CASE_FIXTURE(
+    argument_parser_test_fixture,
+    "add_optional_argument should return an optional argument reference"
+) {
+    const auto& argument = sut.add_optional_argument(name, short_name);
+    REQUIRE(argument.is_optional());
+}
+
 TEST_CASE_FIXTURE(
     argument_parser_test_fixture,
     "add_optional_argument should throw only when adding an"
-    "argument with previously used name"
+    "argument with a previously used name"
 ) {
     sut.add_optional_argument(name, short_name);
 
@@ -87,11 +112,59 @@ TEST_CASE_FIXTURE(
     }
 
     SUBCASE("adding argument with a previously used long name") {
-        REQUIRE_THROWS_AS(sut.add_optional_argument(name), std::invalid_argument);
+        REQUIRE_THROWS_AS(
+            sut.add_optional_argument(name, other_short_name), std::invalid_argument);
     }
 
     SUBCASE("adding argument with a previously used short name") {
         REQUIRE_THROWS_AS(sut.add_optional_argument(other_name, short_name), std::invalid_argument);
+    }
+}
+
+
+TEST_CASE_FIXTURE(
+    argument_parser_test_fixture,
+    "add_flag should return an optional argument reference with flag parameters"
+) {
+    const optional_argument_test_fixture opt_arg_fixture;
+
+    SUBCASE("StoreImplicitly = true") {
+        auto& argument = sut.add_flag(name, short_name);
+
+        REQUIRE(argument.is_optional());
+        REQUIRE_FALSE(sut.value<bool>(name));
+
+        opt_arg_fixture.sut_set_used(argument);
+        REQUIRE(sut.value<bool>(name));
+    }
+
+    SUBCASE("StoreImplicitly = false") {
+        auto& argument = sut.add_flag<false>(name, short_name);
+
+        REQUIRE(argument.is_optional());
+        REQUIRE(sut.value<bool>(name));
+
+        opt_arg_fixture.sut_set_used(argument);
+        REQUIRE_FALSE(sut.value<bool>(name));
+    }
+}
+
+TEST_CASE_FIXTURE(
+    argument_parser_test_fixture,
+    "add_flag should throw only when adding and argument with a previously used name"
+) {
+    sut.add_flag(name, short_name);
+
+    SUBCASE("adding argument with a unique name") {
+        REQUIRE_NOTHROW(sut.add_flag(other_name, other_short_name));
+    }
+
+    SUBCASE("adding argument with a previously used long name") {
+        REQUIRE_THROWS_AS(sut.add_flag(name, other_short_name), std::invalid_argument);
+    }
+
+    SUBCASE("adding argument with a previously used short name") {
+        REQUIRE_THROWS_AS(sut.add_flag(other_name, short_name), std::invalid_argument);
     }
 }
 
