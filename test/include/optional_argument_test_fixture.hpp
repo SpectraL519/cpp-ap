624581eb20087e5a5522fc77240d109ac5dfe7d3
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-12-28 15:41:44 +0100

    PWRS5PZ-36: Add argument functionality - count (#30)
    Added the `argument_parser::count(<argument-name>)` function

--- test/include/optional_argument_test_fixture.hpp
+++ test/include/optional_argument_test_fixture.hpp
@@ -8,7 +8,7 @@
 
 using ap::argument::detail::argument_name;
 using ap::argument::optional_argument;
-using ap::utility::readable;
+using ap::utility::valid_argument_value_type;
 
 namespace ap_testing {
 
@@ -16,15 +16,25 @@ struct optional_argument_test_fixture {
     optional_argument_test_fixture() = default;
     ~optional_argument_test_fixture() = default;
 
-    template <readable T>
+    template <valid_argument_value_type T>
     using value_type = typename optional_argument<T>::value_type;
 
-    template <readable T>
+    template <valid_argument_value_type T>
     inline void sut_set_used(optional_argument<T>& sut) const {
         return sut.set_used();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
+    inline bool sut_is_used(const optional_argument<T>& sut) const {
+        return sut.is_used();
+    }
+
+    template <valid_argument_value_type T>
+    inline std::size_t sut_get_nused(const optional_argument<T>& sut) const {
+        return sut.nused();
+    }
+
+    template <valid_argument_value_type T>
     inline optional_argument<T>& sut_set_value(
         optional_argument<T>& sut, const std::string& str_value
     ) const {
@@ -32,57 +42,57 @@ struct optional_argument_test_fixture {
         return sut.set_value(str_value);
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     inline optional_argument<T>& sut_set_choices(
         optional_argument<T>& sut, const std::vector<value_type<T>>& choices
     ) const {
         return sut.choices(choices);
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline bool sut_has_value(const optional_argument<T>& sut) const {
         return sut.has_value();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline bool sut_has_parsed_values(const optional_argument<T>& sut) const {
         return sut.has_parsed_values();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline std::weak_ordering sut_nvalues_in_range(
         const optional_argument<T>& sut
     ) const {
         return sut.nvalues_in_range();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline const std::any& sut_get_value(
         const optional_argument<T>& sut
     ) const {
         return sut.value();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline const std::vector<std::any>& sut_get_values(
         const optional_argument<T>& sut
     ) const {
         return sut.values();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline const argument_name& sut_get_name(
         const optional_argument<T>& sut
     ) const {
         return sut.name();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline bool sut_is_required(const optional_argument<T>& sut) const {
         return sut.is_required();
     }
 
-    template <readable T>
+    template <valid_argument_value_type T>
     [[nodiscard]] inline const std::optional<std::string>& sut_get_help(
         const optional_argument<T>& sut
     ) const {
