99052faf6a8afc0e5e5c44576170ac1492d48629
SpectraL519 <SpectraL519@users.noreply.github.com>
2025-07-31 13:39:12 +0000

    deploy: eb8033292cbdb34849a9d8ee07b60f110a274411

---
+++ 2.3.0/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['6_20value_20actions_20function_20performed_20after_20parsing_20an_20argument_20s_20value_0',['6. Value actions - Function performed after parsing an argument&apos;s value.',['../md_docs_2tutorial.html#autotoc_md6-value-actions---function-performed-after-parsing-an-arguments-value',1,'']]]
+];
---
+++ 2.3.0/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['default_2ehpp_0',['default.hpp',['../default_8hpp.html',1,'']]]
+];
