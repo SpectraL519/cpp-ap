70ce0021532c259f7b9716b27a8b0a7a8bb0bee1
SpectraL519 <jmusial2003@gmail.com>
2025-08-31 14:36:31 +0200

    exteded argument name tests

--- tests/source/test_argument_name.cpp
+++ tests/source/test_argument_name.cpp
@@ -15,6 +15,7 @@ const auto primary_1_opt = std::make_optional<std::string>(primary_1);
 const auto secondary_1_opt = std::make_optional<std::string>(secondary_1);
 
 const argument_name arg_name_primary_1{primary_1_opt};
+const argument_name arg_name_secondary_1{std::nullopt, secondary_1_opt};
 const argument_name arg_name_full_1{primary_1_opt, secondary_1_opt};
 
 constexpr std::string_view primary_2 = "primary_2";
@@ -24,52 +25,55 @@ const auto primary_2_opt = std::make_optional<std::string>(primary_2);
 const auto secondary_2_opt = std::make_optional<std::string>(secondary_2);
 
 const argument_name arg_name_primary_2{primary_2_opt};
+const argument_name arg_name_secondary_2{std::nullopt, secondary_2_opt};
 const argument_name arg_name_full_2{primary_2_opt, secondary_2_opt};
 
 } // namespace
 
-TEST_CASE("argument_name.primary member should be correctly initialized") {
+TEST_CASE("arugment_name construction should throw if both primary and secondary names are null") {
+    CHECK_THROWS_WITH_AS(
+        (argument_name{std::nullopt, std::nullopt}),
+        "An argument name cannot be empty! At least one of primary/secondary must be specified",
+        std::logic_error
+    );
+}
+
+TEST_CASE("argument_name members should be initialzed properly") {
     CHECK_EQ(arg_name_primary_1.primary, primary_1);
-    CHECK_EQ(arg_name_primary_2.primary, primary_2);
+    CHECK_FALSE(arg_name_primary_1.secondary.has_value());
+
+    CHECK_FALSE(arg_name_secondary_1.primary.has_value());
+    CHECK_EQ(arg_name_secondary_1.secondary, secondary_1);
+
+    CHECK_EQ(arg_name_full_1.primary, primary_1);
+    CHECK_EQ(arg_name_full_1.secondary, secondary_1);
 }
 
-TEST_CASE("argument_name members should be correctly initialized") {
-    REQUIRE_EQ(arg_name_full_1.primary, primary_1);
-
-    REQUIRE(arg_name_full_1.secondary);
-    CHECK_EQ(arg_name_full_1.secondary.value(), secondary_1);
-}
-
-TEST_CASE("operator==(argument_name) should return false if only one argument has both primary and "
-          "secondary values") {
-    CHECK_NE(arg_name_primary_1, arg_name_full_1);
-    CHECK_NE(arg_name_full_1, arg_name_primary_1);
-}
-
-TEST_CASE("operator==(argument_name) should return false if primary names are not equal") {
-    CHECK_NE(arg_name_primary_1, arg_name_primary_2);
-    CHECK_NE(arg_name_full_1, arg_name_full_2);
-}
-
-TEST_CASE("operator==(argument_name) should return false if secondary names are not equal") {
-    CHECK_NE(arg_name_full_1, argument_name{primary_1_opt, secondary_2_opt});
-    CHECK_NE(argument_name{primary_1_opt, secondary_2_opt}, arg_name_full_1);
-}
-
-TEST_CASE("operator==(argument_name) should return true if primary names are equal and both "
-          "secondary names are null") {
+TEST_CASE("operator==(argument_name) should return true only if both primary and secondary names "
+          "have the same values (or no values)") {
+    // exact match
     CHECK_EQ(arg_name_primary_1, arg_name_primary_1);
-}
-
-TEST_CASE("operator==(argument_name) should return true if both primary and secondary names are "
-          "equal") {
+    CHECK_EQ(arg_name_secondary_1, arg_name_secondary_1);
     CHECK_EQ(arg_name_full_1, arg_name_full_1);
+
+    // values of different name members set
+    CHECK_NE(arg_name_full_1, arg_name_primary_1);
+    CHECK_NE(arg_name_full_1, arg_name_secondary_1);
+    CHECK_NE(arg_name_primary_1, arg_name_secondary_1);
+
+    // different values of name members set
+    CHECK_NE(arg_name_primary_1, arg_name_primary_2);
+    CHECK_NE(arg_name_secondary_1, arg_name_secondary_2);
+    CHECK_NE(arg_name_full_1, arg_name_full_2);
 }
 
 TEST_CASE("match(string_view, any) should return true if the given string matches at least one name"
 ) {
     // argument_name with primary name only
     CHECK(arg_name_primary_1.match(primary_1));
+
+    // argument_name with secondary name only
+    CHECK(arg_name_secondary_1.match(secondary_1));
 
     // argument_name with both names
     CHECK(arg_name_full_1.match(primary_1));
@@ -80,6 +84,10 @@ TEST_CASE("match(string_view, any) should return false if the given string dosn'
     // argument_name with primary name only
     CHECK_FALSE(arg_name_primary_1.match(primary_2));
     CHECK_FALSE(arg_name_primary_1.match(secondary_2));
+
+    // argument_name with secondary name only
+    CHECK_FALSE(arg_name_secondary_1.match(primary_2));
+    CHECK_FALSE(arg_name_secondary_1.match(secondary_2));
 
     // argument_name with both names
     CHECK_FALSE(arg_name_full_1.match(primary_2));
@@ -92,6 +100,10 @@ TEST_CASE("match(string_view, primary) should return true only if the given stri
     CHECK(arg_name_primary_1.match(primary_1, argument_name::m_primary));
     CHECK_FALSE(arg_name_primary_1.match(secondary_1, argument_name::m_primary));
 
+    // argument_name with secondary name only
+    CHECK_FALSE(arg_name_secondary_1.match(primary_1, argument_name::m_primary));
+    CHECK_FALSE(arg_name_secondary_1.match(secondary_1, argument_name::m_primary));
+
     // argument_name with both names
     CHECK(arg_name_full_1.match(primary_1, argument_name::m_primary));
     CHECK_FALSE(arg_name_full_1.match(secondary_1, argument_name::m_primary));
@@ -102,6 +114,10 @@ TEST_CASE("match(string_view, secondary) should return true only if the given st
     // argument_name with primary name only
     CHECK_FALSE(arg_name_primary_1.match(primary_1, argument_name::m_secondary));
     CHECK_FALSE(arg_name_primary_1.match(secondary_1, argument_name::m_secondary));
+
+    // argument_name with secondary name only
+    CHECK_FALSE(arg_name_secondary_1.match(primary_1, argument_name::m_secondary));
+    CHECK(arg_name_secondary_1.match(secondary_1, argument_name::m_secondary));
 
     // argument_name with both names
     CHECK_FALSE(arg_name_full_1.match(primary_1, argument_name::m_secondary));
@@ -114,6 +130,10 @@ TEST_CASE("match(argument_name) should return true if either the primary or the 
     CHECK(arg_name_primary_1.match(arg_name_primary_1));
     CHECK(arg_name_primary_1.match(argument_name{primary_2_opt, primary_1_opt}));
 
+    // argument_name with primary name only
+    CHECK(arg_name_secondary_1.match(arg_name_secondary_1));
+    CHECK(arg_name_secondary_1.match(argument_name{secondary_1_opt, secondary_2_opt}));
+
     // argument_name with both names
     CHECK(arg_name_full_1.match(argument_name{primary_1_opt, secondary_2_opt}));
     CHECK(arg_name_full_1.match(argument_name{secondary_1_opt, primary_1_opt}));
@@ -124,6 +144,7 @@ TEST_CASE("match(argument_name) should return true if either the primary or the 
 TEST_CASE("match(argument_name) should return false if neither the primary nor the secondary name "
           "of the passed argument_name matches at least one name") {
     CHECK_FALSE(arg_name_primary_1.match(arg_name_full_2));
+    CHECK_FALSE(arg_name_secondary_1.match(arg_name_full_2));
     CHECK_FALSE(arg_name_full_1.match(arg_name_full_2));
 }
 
@@ -133,6 +154,11 @@ TEST_CASE("operator<< should push correct data to the output stream") {
     SUBCASE("argument_name with primary name only") {
         ss << arg_name_primary_1;
         expected_ss << primary_1;
+    }
+
+    SUBCASE("argument_name with secondary name only") {
+        ss << arg_name_secondary_1;
+        expected_ss << secondary_1;
     }
 
     SUBCASE("argument_name with both names") {
